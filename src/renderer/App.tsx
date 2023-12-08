e61c108304a4eb605a2b53a5ea7b86e233a2468a
liuyizhou <705074065@qq.com>
2023-12-08 18:55:46 +0800

    feat: init sidebar

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -14,44 +14,9 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import router from './router';
 
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </Button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
-
 export default function App() {
+
+
   return (
     <React.StrictMode>
       <RouterProvider router={router} />
